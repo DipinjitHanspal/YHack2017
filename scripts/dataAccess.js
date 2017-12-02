91b841347c9a70e2a23eb6573d07d01a52cf335a
HotSauceToss <hanspal.singhd@gmail.com>
2017-12-02 13:07:54 -0500

    Updated scripting structure

---
+++ scripts/dataAccess.js
@@ -0,0 +1,15 @@
+function search () {
+    url = "https://v3v10.vitechinc.com/solr/v_participant/select?indent=on&wt=json&q=state:Alaska&rows=1";
+    var jsn = getJson();
+};
+
+function getJson() {
+   $.ajax({
+       url : url,
+       method: "GET",
+       success: function(response) {
+           console.log(response);
+           return response; // Return json data
+       }
+   });
+}
