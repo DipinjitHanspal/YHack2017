d5d4dc5bc203845b33180f088e74a2df95fd86f7
Unknown <hanspal.singhd@gmail.com>
2017-12-02 16:39:18 -0500

    Pre-nap WIP
    Plot some stuff

--- scripts/dataAccess.js
+++ scripts/dataAccess.js
@@ -9,18 +9,38 @@ function search () {
 function getJsn () {
     var stateArray = [];
     var idArray = [];
-    url = "https://v3v10.vitechinc.com/solr/v_participant/select?indent=on&wt=json&q=*:*&rows=10";    
+    url = "https://v3v10.vitechinc.com/solr/v_participant/select?indent=on&wt=json&q=*:*&rows=100&omitHeader=true";    
     $.getJSON(url, function(data){
         var qresult = data.response.docs;
         for (var x = 0; x < qresult.length; x ++){
-            console.log(qresult[x])
-             stateArray += qresult[x]['state'] + ", ";
-             idArray += qresult[x]['id'] + ", ";
+            // y = ConvertToCSV(JSON.stringify(data));
+            
+            // console.log(qresult[x])
+            stateArray += qresult[x]['state'] + ", ";
+            idArray += qresult[x]['id'] + ", ";
         }
-        console.log(stateArray.toString());
-        console.log(idArray.toString());
+        console.log(ConvertToCSV(qresult));
+        // console.log(stateArray.toString());
+        // console.log(idArray.toString());
     });
 
+    function ConvertToCSV(objArray) {
+        var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
+        var str = '';
+        for (var i = 0; i < array.length; i++) {
+            var line = '';
+            for (var index in array[i]) {
+                // if (line != '') line += ','
+                if (array[i][index] == ''){
+                    line += " ,"
+                } else{
+                    line += array[i][index] + ",";
+                } 
+            }
+            str += line + '\r\n';
+        }
+        return str;
+    }
     //     $.ajax({
 //        url : url,
 //        method: "GET",
